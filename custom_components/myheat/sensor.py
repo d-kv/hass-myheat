63e2c5501d7f545a83cdec3118e48841d853c3ba
Dmitry Kuznetsov <d.kuznetsov12@tinkoff.ru>
2023-05-09 21:16:41 +0000

    it works

--- custom_components/myheat/sensor.py
+++ custom_components/myheat/sensor.py
@@ -1,6 +1,7 @@
 """Sensor platform for MyHeat."""
 
 from homeassistant.components.sensor import SensorEntity, SensorDeviceClass
+from homeassistant.core import HomeAssistant, callback
 
 import logging
 
@@ -32,18 +33,42 @@ class MhSensor(MhEntity, SensorEntity):
 
     def __init__(self, coordinator, config_entry, heater):
         super().__init__(coordinator, config_entry)
-        self.heater = heater
-        self.update()
+        self.heater_id = heater["id"]
+        _logger.info(f"Setup up pressure sensor for heater id {self.heater_id}")
 
     @property
     def name(self):
         """Return the name of the sensor."""
         return f"{DEFAULT_NAME}_{SENSOR}_pressure"
 
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this entity."""
+        return f"{super().unique_id}_heater_{self.heater_id}"
+
     async def async_update(self) -> None:
         """Update the state of the sensor."""
+        self._update_heater_pressure()
+
+    @callback
+    def _handle_coordinator_update(self):
+        """Get the latest state from the thermostat."""
+
+        self._update_heater_pressure()
+
+        self.async_write_ha_state()
+
+    def _update_heater_pressure(self):
+        heater = self._heater()
+        if heater is None:
+            _logger.error(f"Heater with id {self.heater_id} is missing")
+            return
+        _logger.info(f"Updating pressure sensor entries: {heater}")
+        self._attr_native_value = heater["pressure"]
+
+    def _heater(self) -> dict | None:
         heaters = self.coordinator.data.get("data", {}).get("heaters", [])
-        _logger.info(f"Updating pressure sensor entries: {heaters}")
         for heater in heaters:
-            if heater["id"] == self.heater["id"]:
-                self.native_value = heater["pressure"]
+            if heater["id"] == self.heater_id:
+                return heater
+        return None
